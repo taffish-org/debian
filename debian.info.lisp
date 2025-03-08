de7d99272d27a8e30b5b01889ac5c06889b41ae1
Kaiyuan Han <hermit_han@163.com>
2025-03-08 16:02:51 +0800

    Initial commit

---
+++ debian.info.lisp
@@ -0,0 +1,7 @@
+(:name           "debian")
+(:repo           "taffish-org/debian")
+(:author         "KaiyuanHan")
+(:version        "12")
+(:intro          "Debian (/ˈdɛbiən/), also known as Debian GNU/Linux, is a free and open source Linux distribution, developed by the Debian Project, which was established by Ian Murdock in August 1993. Debian is one of the oldest operating systems based on the Linux kernel, and is the basis of many other Linux distributions.")
+(:dependencies)
+(:functions      "[default debian 12]")
